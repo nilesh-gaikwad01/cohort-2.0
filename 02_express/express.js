f485447b105e63d22523c6d2fcef5e8a4f104e72
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-10-11 18:44:34 +0530

    create first express server

---
+++ 02_express/express.js
@@ -0,0 +1,40 @@
+
+
+
+// create http sever with express
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// First route
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+Post
+
+app.post("/user", (req, res) => {
+    const {name, age} = req.body;
+    res.json({message :`User ${name} is ${age} year old`})
+});
+
+app.post("/contact", (req, res) => {
+  res.send("Contact form submitted")
+});
+
+app.put ("/user/:id", (req, res) => {
+  const updatedData = req.body
+  console.log(updatedData);
+  
+  res.send(`User with Id ${req.params.id} Updated`)
+}); 
+
+app.delete("user/:id", (req, res) => {
+  res.send(`User with Id ${req.params.id} deleted`)
+});
+
+app.listen(6000, () => {
+  console.log("Server running at http://localhost:3003");
+});
