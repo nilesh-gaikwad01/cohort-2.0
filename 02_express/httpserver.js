90846cb4e9e53dc00d27d01ced467d2b132e20c6
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-10-11 18:42:30 +0530

    create http server

---
+++ 02_express/httpserver.js
@@ -0,0 +1,13 @@
+// Create  server with http module
+
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello ");
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
