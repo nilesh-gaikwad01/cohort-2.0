d21e8fb361bc70049b5fed47dbfecf0174a947f3
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-11-11 23:19:23 +0530

    feat: Add login UI and display user data using MongoDB

---
+++ 03_mongodb/login_app/db.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const jwtPassword = "123456"
+const PORT = 1020; 
+// connect to the DB
+  mongoose.connect("mongodb+srv://nileshngaikwad96_db_user:Nomad123@cluster0.jnuh08l.mongodb.net/authdb")
+
+// Create User Schema
+
+const userSchema = new mongoose.Schema({
+    username : String,
+    password:  String
+});
+
+// Create User model 
+
+const User = mongoose.model("User" , userSchema); 
+
+//New Registration routes
+
+app.post("/signUp", async (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const newUser = new User({username,password});
+   await newUser.save(); // Save in the db
+
+res.json({msg : "User is registred sucessfully"})
+
+});
+
+ app.post("/signIn", async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check if the user is already in db
+
+const user = await User.findOne({username, password});
+
+    if (!user) {
+        return res.status(403).json({
+            msg : "User is not found" 
+        }) 
+    }
+
+    const token = jwt.sign({username: username}, jwtPassword);
+    return res.json({
+        token,
+    })
+ });
+
+ app.get("/users", async (req, res) => {
+    const token = req.headers.authorization;
+
+    try {
+        const decoded = jwt.verify(token, jwtPassword);
+        const username = decoded.username;
+        // return all users instead of logged in
+        const users = await User.find({username :{$ne: username}})
+        res.send(users);
+    } catch (err) {
+        return res.status(403).json({
+            msg : "invalid token"
+        });
+        
+    }
+ });
+
+ app.get("/", (req, res) => {
+    res.send("Sucessfully Mongodb and server running ")
+ });
+
+ app.listen(PORT, () => {
+    console.log("Server is running on the Port : http://localhost:1020/");
+    
+ })
---
+++ 03_mongodb/login_app/readme.md
@@ -0,0 +1,11 @@
+## login page with JWT Authentication In MongoDB database
+
+
+Added a new login page and backend logic for user authentication.
+This commit includes:
+- Frontend form for username/password input.
+- Backend API endpoint to handle login requests.
+- Connection and data storage using MongoDB.
+- Basic validation and password hashing ().
+
+
